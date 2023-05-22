65efec7f6e03fbc81900e8996a79704b7ae274af
yltyadi <adi.yeltay@nu.edu.kz>
2023-05-22 18:33:33 +0600

    added orders, order_items, clients paths

--- controllerFunctions.php
+++ controllerFunctions.php
@@ -75,3 +75,175 @@ function deleteProduct($connect, $id) {
     http_response_code(200);
     echo json_encode($response);
 }
+
+function getOrders($connect) {
+    $orders = mysqli_query($connect, "SELECT * FROM `orders`");
+    $ordersArr = [];
+
+    if ($connect) {
+        while ($order = mysqli_fetch_assoc($orders)) {
+            $ordersArr[] = $order;
+        }
+
+        echo json_encode($ordersArr);
+    } else {
+        echo "Error! Failed to connect to database!";
+    }
+}
+
+function getOrderById($connect, $id) {
+    $order = mysqli_query($connect, "SELECT * FROM `orders` WHERE `order_id`='$id'");
+    
+    if (mysqli_num_rows($order) !== 0) {
+        $order = mysqli_fetch_assoc($order);
+        echo json_encode($order);
+    } else {
+        $response = [
+            'status' => false,
+            'message' => 'Order not found'
+        ];
+
+        http_response_code(404);
+        echo json_encode($response);
+    }
+}
+
+function addOrder($connect, $id, $duration) {
+    mysqli_query($connect, "INSERT INTO `Orders` (`order_id`, `client_id`, `duration_weeks`) VALUES (NULL, '$id', '$duration')");
+
+    $response = [
+        'status' => true,
+        'message' => mysqli_insert_id($connect)
+    ];
+
+    http_response_code(201);
+    echo json_encode($response);
+}
+
+function deleteOrder($connect, $id) {
+    mysqli_query($connect, "DELETE FROM `Orders` WHERE `Orders`.`order_id` = '$id'");
+
+    $response = [
+        'status' => true,
+        'message' => 'Order was deleted'
+    ];
+
+    http_response_code(200);
+    echo json_encode($response);
+}
+
+function getItems($connect) {
+    $items = mysqli_query($connect, "SELECT * FROM `order_items`");
+    $itemsArr = [];
+
+    if ($connect) {
+        while ($item = mysqli_fetch_assoc($items)) {
+            $itemsArr[] = $item;
+        }
+
+        echo json_encode($itemsArr);
+    } else {
+        echo "Error! Failed to connect to database!";
+    }
+}
+
+function getItemById($connect, $id) {
+    $item = mysqli_query($connect, "SELECT * FROM `order_items` WHERE `order_id`='$id'");
+    
+    if (mysqli_num_rows($item) !== 0) {
+        $item = mysqli_fetch_assoc($item);
+        echo json_encode($item);
+    } else {
+        $response = [
+            'status' => false,
+            'message' => 'Item not found'
+        ];
+
+        http_response_code(404);
+        echo json_encode($response);
+    }
+}
+
+function addItem($connect, $client_id, $product_id, $quantity) {
+    mysqli_query($connect, "INSERT INTO `Order_Items` (`item_id`, `order_id`, `product_id`, `quantity`) VALUES (NULL, '$client_id', '$product_id', '$quantity')");
+
+    $response = [
+        'status' => true,
+        'message' => mysqli_insert_id($connect)
+    ];
+
+    http_response_code(201);
+    echo json_encode($response);
+}
+
+function deleteItem($connect, $id) {
+    mysqli_query($connect, "DELETE FROM `Order_Items` WHERE `Order_Items`.`item_id` = '$id'");
+
+    $response = [
+        'status' => true,
+        'message' => 'Order Item was deleted'
+    ];
+
+    http_response_code(200);
+    echo json_encode($response);
+}
+
+function getClients($connect) {
+    $clients = mysqli_query($connect, "SELECT * FROM `clients`");
+    $clientsArr = [];
+
+    if ($connect) {
+        while ($client = mysqli_fetch_assoc($clients)) {
+            $clientsArr[] = $client;
+        }
+
+        echo json_encode($clientsArr);
+    } else {
+        echo "Error! Failed to connect to database!";
+    }
+}
+
+function getClientById($connect, $id) {
+    $client = mysqli_query($connect, "SELECT * FROM `clients` WHERE `client_id`='$id'");
+    
+    if (mysqli_num_rows($client) !== 0) {
+        $client = mysqli_fetch_assoc($client);
+        echo json_encode($client);
+    } else {
+        $response = [
+            'status' => false,
+            'message' => 'Client not found'
+        ];
+
+        http_response_code(404);
+        echo json_encode($response);
+    }
+}
+
+function addClient($connect, $data) {
+    $client_name = $data['client_name'];
+    $phone_number = $data['phone_number'];
+    $address = $data['address'];
+
+    mysqli_query($connect, "INSERT INTO `Clients` (`client_id`, `client_name`, `phone_number`, `address`) VALUES (NULL, '$client_name', '$phone_number', '$address')");
+
+    $response = [
+        'status' => true,
+        'message' => mysqli_insert_id($connect)
+    ];
+
+    http_response_code(201);
+    echo json_encode($response);
+}
+
+function deleteClient($connect, $id) {
+    mysqli_query($connect, "DELETE FROM `Clients` WHERE `Clients`.`client_id` = '$id'");
+
+    $response = [
+        'status' => true,
+        'message' => 'Client was deleted'
+    ];
+
+    http_response_code(200);
+    echo json_encode($response);
+}
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-// Note: This project was ran and tested on APACHE SERVER
+// Note: This project was ran and tested on APACHE SERVER using XAMPP on MacOS
 
 require "controllerFunctions.php";
 
@@ -15,48 +15,163 @@ $fullpath = explode('/', $_GET['q']);
 $path = $fullpath[0];
 $id = $fullpath[1];
 
-switch ($method) {
-    case "GET":
-        if ($path === 'products') {
+if ($path === "products") {
+    switch ($method) {
+        case "GET":
             if (isset($id)) {
                 getProductById($connect, $id);
             } else {
                 getProducts($connect);
             }
-        } else {
-            $response = [
-                'status' => false,
-                'message' => 'Path not found'
-            ];
-        
-            http_response_code(404);
-            echo json_encode($response);
-        }
-        break;
-    case "POST":
-        if ($path === 'products') {
+            break;
+        case "POST": 
             addProduct($connect, $_POST);
-        }
-        break;
-    case "PATCH":
-        if ($path === 'products') {
+            break;
+        case "PATCH": // PATCH method only accepts data in raw json format when updating in postman
             if (isset($id)) {
                 $data = json_decode(file_get_contents('php://input'), true);
                 updateProduct($connect, $id, $data);
+            } else {
+                http_response_code(404);
+                echo json_encode([
+                    'status' => false,
+                    'message' => 'Missing id'
+                ]);
             }
-        }
-        break;
-    case "DELETE":
-        if ($path === 'products') {
+            break;
+        case "DELETE":
             if (isset($id)) {
                 deleteProduct($connect, $id);
+            } else {
+                http_response_code(404);
+                echo json_encode([
+                    'status' => false,
+                    'message' => 'Missing id'
+                ]);
             }
-        }
-        break;
-    default:
-        http_response_code(404);
-        echo json_encode([
-            'status' => false,
-            'message' => 'Request method not found'
-        ]);
+            break;
+        default:
+            http_response_code(404);
+            echo json_encode([
+                'status' => false,
+                'message' => 'Request method not found'
+            ]);
+    }
+} elseif ($path === "orders") {
+    $duration = $fullpath[2];
+    switch ($method) {
+        case "GET":
+            if (isset($id)) {
+                getOrderById($connect, $id);
+            } else {
+                getOrders($connect);
+            }
+            break;
+        case "POST":
+            if (isset($id) && isset($duration)) {
+                addOrder($connect, $id, $duration);;
+            } else {
+                http_response_code(404);
+                echo json_encode([
+                    'status' => false,
+                    'message' => 'Client id or Duration are missing'
+                ]);
+            }
+            break;
+        case "DELETE":
+            if (isset($id)) {
+                deleteOrder($connect, $id);
+            } else {
+                http_response_code(404);
+                echo json_encode([
+                    'status' => false,
+                    'message' => 'Missing id'
+                ]);
+            }
+            break;
+        default:
+            http_response_code(404);
+            echo json_encode([
+                'status' => false,
+                'message' => 'Request method not found'
+            ]);
+    }
+} elseif ($path === "order_items") {
+    $product_id = $fullpath[2];
+    $quantity = $fullpath[3];
+    switch ($method) {
+        case "GET":
+            if (isset($id)) {
+                getItemById($connect, $id);
+            } else {
+                getItems($connect);
+            }
+            break;
+        case "POST":
+            if (isset($id) && isset($product_id) && isset($quantity)) {
+                addItem($connect, $id, $product_id, $quantity);;
+            } else {
+                http_response_code(404);
+                echo json_encode([
+                    'status' => false,
+                    'message' => 'Client id, Product id, or Quantity are missing'
+                ]);
+            }
+            break;
+        case "DELETE":
+            if (isset($id)) {
+                deleteItem($connect, $id);
+            } else {
+                http_response_code(404);
+                echo json_encode([
+                    'status' => false,
+                    'message' => 'Missing id'
+                ]);
+            }
+            break;
+        default:
+            http_response_code(404);
+            echo json_encode([
+                'status' => false,
+                'message' => 'Request method not found'
+            ]);
+    }
+} elseif ($path === "clients") {
+    switch ($method) {
+        case "GET":
+            if (isset($id)) {
+                getClientById($connect, $id);
+            } else {
+                getClients($connect);
+            }
+            break;
+        case "POST":
+            addClient($connect, $_POST);
+            break;
+        case "DELETE":
+            if (isset($id)) {
+                deleteClient($connect, $id);
+            } else {
+                http_response_code(404);
+                echo json_encode([
+                    'status' => false,
+                    'message' => 'Missing id'
+                ]);
+            }
+            break;
+        default:
+            http_response_code(404);
+            echo json_encode([
+                'status' => false,
+                'message' => 'Request method not found'
+            ]);
+    }
+} else {
+    $response = [
+        'status' => false,
+        'message' => 'Path not found'
+    ];
+
+    http_response_code(404);
+    echo json_encode($response);
 }
